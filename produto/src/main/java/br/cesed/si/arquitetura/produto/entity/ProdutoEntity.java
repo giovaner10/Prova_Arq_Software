b5290e62819d38e9b609326756e6a5126d6b588b
giovaner10 <giovanerr10@gmail.com>
2022-03-28 20:37:37 -0300

    service entity

---
+++ produto/src/main/java/br/cesed/si/arquitetura/produto/entity/ProdutoEntity.java
@@ -0,0 +1,29 @@
+package br.cesed.si.arquitetura.produto.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class ProdutoEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "Nome", nullable = false)
+    private String nome;
+
+
+
+}
