23d16da76adbb9f29fe3ed022ae8a9f0470e329a
giovaner10 <giovanerr10@gmail.com>
2022-04-01 20:37:22 -0300

    atualizada no PorteiroApplication

--- APIGateway/src/main/java/br/cesed/si/arquitetura/porteiro/PorteiroApplication.java
+++ APIGateway/src/main/java/br/cesed/si/arquitetura/porteiro/PorteiroApplication.java
@@ -2,8 +2,11 @@ package br.cesed.si.arquitetura.porteiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PorteiroApplication {
 
 	public static void main(String[] args) {
